d8ed534f7075aba53a7ab311bf19dd5e752c7c1b
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-01-04 11:26:58 -0500

    Initial Commit

---
+++ TicTacToeUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToeUI
+//
+//  Created by Adrian McDaniel on 1/4/17.
+//  Copyright © 2017 dssafsfsd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
